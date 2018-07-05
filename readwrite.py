7903edbc33f2fe8abe352b13b2fb872c351777ab
akar2014 <40709604+akar2014@users.noreply.github.com>
2018-07-05 10:11:55 -0400

    Add files via upload

--- readwrite.py
+++ readwrite.py
@@ -1,38 +1,36 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 27 13:25:19 2018
-
-@author: amankar
-
-
 Attempting to Reading in Raw GUPPI File, FFT the Data
 IFFT The Data, and write the file back in the original raw format
 """
 
 
 
-from astropy.io import fits
 import numpy as np
 import pylab as plt
-from matplotlib import rcParams
-import time
+import os
 from GbtRaw import *
 import scipy.stats as stats
-
+from shutil import copyfile
 
 
 
 def main():
-    blocks=5
+    blocks=2
     nfreq=1024
-    nspec=618
+    nspec=53
     nint=159
-    pol=0
+    
     path    = '/Users/amankar/Downloads/realTimeRfi-master/'
-    in_file = 'guppi_56465_J1713+0747_0006.0000.raw'
-    fitsRoot = '0006.0000'
-    g = GbtRaw(path+in_file)
+    
+    in_file = 'backup.raw'
+    out_file = 'guppi_56465_J1713+0747_0006.0000.raw'
+
+    copyfile(path+in_file,path+out_file)
+    
+    g = GbtRaw(path+out_file,update=True)
+    
     n = g.get_num_blocks()
     print "the file has", n, "blocks"
     
@@ -40,48 +38,72 @@ def main():
     obsbw   = g.header_dict['OBSBW']
     obsnchan= g.header_dict['OBSNCHAN']
     
-    input_tsData = g.extract(0,blocks, overlap=False)
-    output_tsData=np.empty_like(input_tsData)
+    for block in range(blocks):        
+        
+        header,input_tsData=g.get_block(block)
+        #input_tsData = g.extract(0,blocks, overlap=True)
+        output_tsData=np.empty_like(input_tsData)
+        print "Prcoessing Block: ", (block+1)
     
-    sp = 2*pol
-    ep = sp+2
-
-    for chan in range(32):
-        print "Processing channel: ", chan
-
-        fStart  = obsfreq - float(obsbw)/2.0 + (chan *  float(obsbw) / obsnchan)
-        fRes = float(obsbw) / (nfreq * obsnchan)
-
-        tsamp   = abs(float(obsnchan)/obsbw) * 1.0e-06  # MHz to Hz
-        tStart  = 0
-        tRes    = tsamp * nfreq * nint
-
-        input_chanData = input_tsData[chan, :, sp:ep]
-        output_chanData=np.empty_like(input_chanData)
-        print fStart,fRes,tsamp,tRes
-        print input_chanData
+        for chan in range(32):
+            
+            print "Processing Channel: ", (chan+1)
+            fStart  = obsfreq - float(obsbw)/2.0 + (chan *  float(obsbw) / obsnchan)
+            fRes = float(obsbw) / (nfreq * obsnchan)
+    
+            tsamp   = abs(float(obsnchan)/obsbw) * 1.0e-06  # MHz to Hz
+            tStart  = 0
+            tRes    = tsamp * nfreq * nint
+            
+            for pol in range(2):
+                sp=2*pol
+                ep=sp+2
         
-        for s in range(nspec):
-
-            winStart = s * (nfreq * nint)
-
-            for i in range(nint):
-
-                start = winStart + i * nfreq
-                end = start + nfreq
-
-                in_arr = np.zeros((nfreq), dtype=np.complex_)
-                out_arr = np.zeros((nfreq), dtype=np.complex_)
-                in_arr.real = input_chanData[start:end, 0]
-                in_arr.imag = input_chanData[start:end, 1]
+                input_chanData = input_tsData[chan, :, sp:ep]
+                output_chanData=np.empty_like(input_chanData)
+               # print fStart,fRes,tsamp,tRes
+               # print input_chanData
                 
-                out_arr = np.fft.ifft(np.fft.fft(in_arr))
-                
-                output_chanData[start:end,0]=out_arr.real
-                output_chanData[start:end,1]=out_arr.imag
+                for s in range(nspec):
         
-        print output_chanData
-        output_tsData[chan,:,sp:ep]=output_chanData
+                    winStart = s * (nfreq * nint)
+        
+                    for i in range(nint):
+        
+                        start = winStart + i * nfreq
+                        end = start + nfreq
+                        in_arr = np.zeros((nfreq), dtype=np.complex_)
+                        out_arr = np.zeros((nfreq), dtype=np.complex_)
+                        
+                        if(start>input_tsData.shape[1]):
+                            break
+                        if((end>input_tsData.shape[1])):
+                            end=input_tsData.shape[1]
+                            in_arr = np.zeros((end-start), dtype=np.complex_)
+                            out_arr = np.zeros((end-start), dtype=np.complex_)
+                            in_arr.real = input_chanData[start:end, 0]
+                            in_arr.imag = input_chanData[start:end, 1]
+                            out_arr = np.fft.ifft(np.fft.fft(in_arr,norm='ortho'),norm='ortho')
+                            output_chanData[start:end,0]=out_arr.real
+                            output_chanData[start:end,1]=out_arr.imag
+                            break
+                        
+                        in_arr.real = input_chanData[start:end, 0]
+                        in_arr.imag = input_chanData[start:end, 1]
+                        
+                        out_arr = np.fft.ifft(np.fft.fft(in_arr,norm='ortho'),norm='ortho')
+                        
+                        output_chanData[start:end,0]=out_arr.real
+                        output_chanData[start:end,1]=out_arr.imag
+                
+    
+                #print output_chanData
+                output_tsData[chan,:,sp:ep]=output_chanData
+                
+        g.put_block(header,output_tsData,block)
+    
+    
+    
         
                 
                 
